b9a9473ca984df138630616cd6a52e899a0359e7
layonthehorn <47235696+layonthehorn@users.noreply.github.com>
2019-02-02 19:15:28 -0800

    Added user mode selection

--- fileremove.py
+++ fileremove.py
@@ -1,17 +1,39 @@
 #!/usr/bin/env python3
 
 # layonthehorn
-
 import sys, re, os
 
+# creating a usage statment for users
+usagestatment ="""Usage: {0} cwd|abs searchpattern""".format(sys.argv[0]) 
+
+# checking if the user supplied the right number of arguments
 if len(sys.argv) < 3:
-    print("""Usage: {0} FILEPATH FILEEXTENSION""".format(sys.argv[0]))
+    print(usagestatment)
     sys.exit(0)
 
-# Saves the users path to search
-filepath = sys.argv[1]
-# Saves the regular expression to search by
+# Saves the users operation mode 
+opmode= sys.argv[1]
+
+# if the user select cwd then the program used their pwd
+if opmode.lower() == "cwd":
+    userpwd = os.getcwd()
+
+# if the user selected abs they must supply a absolute path
+elif opmode.lower() == "abs":
+# asks the user for a path and checks that is is an absolute one 
+    userpwd = input("Enter the absolute path. ")
+# if the check returns false the program exits
+    if not (re.search(r"^/",userpwd)):
+        print("Must be an absolute path")
+        sys.exit(0)
+# exits if the user did not enter a accepted op mode
+else:
+    print(usagestatment)
+    sys.exit(0)
+
+# saving the search pattern 
 searchpat = sys.argv[2]
+
 # compiles the users regex for faster searching
 searchreg = re.compile(r"{0}$".format(searchpat))
 # creates an empty list to store the matched files
@@ -20,7 +42,7 @@ filelist = []
 dirlist = []
 
 # this searches through the file path supplied
-for direct, dirname, files in os.walk(filepath):
+for direct, dirname, files in os.walk(userpwd):
 # Goes through the list of files in a directory
     for mfile in files:
 # checks if any files match the regular expression
